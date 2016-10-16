bbcda3e2fd7964cedf8e83be714a039807257a3a
mariahsiao <thatsjustmeiwant@gmail.com>
2016-10-16 15:59:21 +0800

    add comment
    缺少樓號

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user, only: [:new, :edit, :create, :update, :destroy]
 
   def index
     @posts = Post.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :comments
   devise_for :users
-  resources :posts
+  resources :posts do
+    member do
+    end
+    resources :comments
+  end
+
   root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
