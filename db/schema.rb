6b407a372e83fb1982086b65813c73fcc9b94c21
mariahsiao <thatsjustmeiwant@gmail.com>
2016-10-16 20:24:01 +0800

    add migration name of devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016063318) do
+ActiveRecord::Schema.define(version: 20161016080540) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20161016063318) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
