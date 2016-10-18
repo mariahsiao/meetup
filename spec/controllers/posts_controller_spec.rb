1a72c86569763ec6537a4301a6195e86dd68bffc
mariahsiao <thatsjustmeiwant@gmail.com>
2016-10-18 14:11:12 +0800

    rspec #create

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -36,23 +36,21 @@ RSpec.describe PostsController, type: :controller do
 
   describe "GET new" do
 
-    context "when user login" do
-      it "assign @post" do
-        user = FactoryGirl.create(:user)
-        sign_in user
+    let(:user) { FactoryGirl.create(:user) }
+    let(:post) { FactoryGirl.build(:post) }
 
-        post = FactoryGirl.build(:post)
+    context "when user login" do
+      before do
+        sign_in user
         get :new
+      end
+
+      it "assign @post" do
         expect(assigns(:post)).to be_new_record
         expect(assigns(:post)).to be_instance_of(Post)
       end
 
       it "render_template" do
-        user = FactoryGirl.create(:user)
-        sign_in user
-
-        post = FactoryGirl.build(:post)
-        get :new
         expect(response).to render_template("new")
       end
     end
@@ -60,8 +58,52 @@ RSpec.describe PostsController, type: :controller do
     context "when user not login" do
       it "redirect_to new_user_session_path" do
         get :new
+
         expect(response).to redirect_to new_user_session_path
       end
+    end
+  end
+
+  describe "POST create" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    context "when course doesn't have a title" do
+      before { sign_in_user }
+
+      it "doesn't create a record" do
+        expect{ post :create, post: { :description => "bar" }}.to change{Course.count}.by(0)
+      end
+
+      it "render new_template"
+        post :create, post: { :description => "bar" }
+
+        expect(response).to render_template("new")
+      end
+    end
+
+    context "when post have a title" do
+      befoer {sign_in_user}
+
+      it "create a new post record" do
+        post = FactoryGirl.build(:post)
+
+        expect{ post :create, post: FactoryGirl.attributes_for(:post)}.to change{ Course.count}.by(1)
+      end
+
+      it "redirect to posts_path" do
+        post = FactoryGirl.build(:post)
+
+        post :create, course: FactoryGirl.attributes_for(:post)
+
+        expect(response).to redirect_to posts_path
+      end
+    end
+
+    it_behaves_like "requrire_sign_in" do
+      let(:action) {
+        post = FactoryGirl.build(:course)
+        post :create, post: FactoryGirl.attributes_for(:post)
+      }
     end
   end
 
